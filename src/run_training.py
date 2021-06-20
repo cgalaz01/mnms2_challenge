2d88732b10b9a3edce65d2a2fe968fb31ab752aa
cgalaz01 <c.galazis20@imperial.ac.uk>
2021-06-20 16:11:14 +0100

    Added visual debug summary

--- src/run_training.py
+++ src/run_training.py
@@ -1,5 +1,8 @@
 import os
 
+from typing import Union
+from pathlib import Path
+
 import datetime
 import random
 
@@ -10,8 +13,10 @@ from tensorflow import keras
 
 from tensorboard.plugins.hparams import api as hp
 
+import matplotlib.pyplot as plt
+
 from configuration import HyperParameters
-from data import TensorFlowDataGenerator
+from data import TensorFlowDataGenerator, DataGenerator
 from tf.models import multi_stage_model
 from tf.losses.loss import FocalLoss, TverskyLoss
 from tf.metrics.metrics import dice
@@ -27,7 +32,7 @@ np.random.seed(__SEED)
 def get_callbacks(prefix: str, checkpoint_directory: str, hparams):
     model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_directory,
-        save_weights_only=False,
+        save_weights_only=True,
         monitor='val_loss',
         mode='min',
         save_best_only=True)
@@ -41,6 +46,44 @@ def get_callbacks(prefix: str, checkpoint_directory: str, hparams):
     return [model_checkpoint_callback,
             tensorboard_callback,
             hparams_callback]
+
+
+def visual_summary(model: keras.Model, generator: DataGenerator,
+                   output_path: Union[str, Path]) -> None:
+    os.makedirs(output_path, exist_ok=True)
+    i = 0
+    for data in generator.validation_affine_generator():
+        # Add batch dimension to each of the input values
+        for key, value in data[0].items():
+            data[0][key] = np.expand_dims(value, 0)
+        
+        prediction_list = model.predict(data[0])
+        prediction_dict = {name: pred for name, pred in zip(model.output_names, prediction_list)}
+
+        predict_sa = np.argmax(prediction_dict['output_sa'][0], axis=-1).astype(np.uint8)
+        gt_sa = np.argmax(data[1]['output_sa'])
+        
+        predict_la = np.argmax(prediction_dict['output_la'][0], axis=-1).astype(np.uint8)
+        gt_la = np.argmax(data[1]['output_la'])
+        
+        output_file = os.path.join(output_path, str(i) + '.png')
+        i += 1
+        
+        plt.subplot(221)
+        plt.title('SA GT')
+        plt.imshow(gt_sa[..., gt_sa.shape[-1] // 2])
+        plt.subplot(222)
+        plt.title('SA Prediction')
+        plt.imshow(predict_sa[..., gt_sa.shape[-1] // 2])
+        plt.subplot(223)
+        plt.title('LA GT')
+        plt.imshow(gt_la)
+        plt.subplot(224)
+        plt.title('LA Prediction')
+        plt.imshow(predict_la)
+        
+        plt.savefig(output_file)
+        plt.close()
 
 
 if __name__ == '__main__':
@@ -94,3 +137,6 @@ if __name__ == '__main__':
                   callbacks=get_callbacks(prefix, checkpoint_path, hparams),
                   verbose=1)
         
+        model.load_weights(checkpoint_path)
+        visual_summary(model, data_gen, 'tmp/output_results')
+
