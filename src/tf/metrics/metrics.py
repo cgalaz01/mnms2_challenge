9c3f2b27a68812156b36e4887c8b533f161b6846
cgalaz01 <c.galazis20@imperial.ac.uk>
2021-06-27 17:26:52 +0100

    Updated Dice metric

--- src/tf/metrics/metrics.py
+++ src/tf/metrics/metrics.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras import backend as K
 
 
 @tf.autograph.experimental.do_not_convert
-def dice(y_true, y_pred, ignore_background=True, right_ventricle_only=True):
+def dice(y_true, y_pred):
     epsilon = 1e-6
     
     y_true = tf.cast(y_true, dtype=tf.float32)
@@ -11,19 +12,10 @@ def dice(y_true, y_pred, ignore_background=True, right_ventricle_only=True):
     # Expected y_pred to be 'logits'
     y_pred = tf.sigmoid(y_pred)
     
-    # Threshold values
-    y_pred = tf.cast(tf.cast(y_pred + 0.5, dtype=tf.uint8), dtype=tf.float32)
-    
-    dim = tf.reduce_prod(tf.shape(y_true)[1:])
-    y_true_flatten = tf.reshape(y_true, [-1, dim])
-    y_pred_flatten = tf.reshape(y_pred, [-1, dim])
-
-    intersection = tf.math.reduce_sum(y_true_flatten * y_pred_flatten, axis=-1)
-    
-    union = tf.math.reduce_sum(y_true_flatten, axis=-1) + \
-        tf.math.reduce_sum(y_pred_flatten, axis=-1)
-    
-    dice_coef = tf.math.reduce_mean((2. * intersection + epsilon) / (union + epsilon))
+    y_true = K.flatten(y_true)
+    y_pred = K.flatten(y_pred)
+    intersection = K.sum(y_true * y_pred)
+    dice_coef = (2. * intersection + epsilon) / (K.sum(y_true) + K.sum(y_pred) + epsilon)
 
     return dice_coef
 
