e82fa98ad5a0fa4f506e6070476ec35d1e0c15b6
cgalaz01 <c.galazis20@imperial.ac.uk>
2021-06-27 14:09:39 +0100

    Updated model

--- src/tf/models/multi_stage_model.py
+++ src/tf/models/multi_stage_model.py
@@ -5,113 +5,6 @@ from tensorflow.keras import layers
 
 from tf.layers.transformer import spatial_target_transformer
 
-
-
-def _inception_block_a(x, num_filters, kernel_initializer, suffix, index):
-    # Branch 1
-    x1 = layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='same',
-                             name=suffix + '_inception_a_max_pooling_1_1_' + index)(x)
-    x1 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_1_2_' + index)(x1)
-    x1 = layers.Activation('relu', name=suffix + '_inception_a_activation_1_3_' + index)(x1)
-    
-    # Branch 2
-    x2 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_2_1_' + index)(x)
-    x2 = layers.Activation('relu', name=suffix + '_inception_a_activation_2_2_' + index)(x2)
-    x2 = layers.Conv2D(num_filters // 2, (3, 3), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_2_3_' + index)(x2)
-    x2 = layers.Activation('relu', name=suffix + '_inception_a_activation_2_4_' + index)(x2)
-    
-    # Branch 3
-    x3 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_3_1_' + index)(x)
-    x3 = layers.Activation('relu', name=suffix + '_inception_a_activation_3_2_' + index)(x3)
-    x3 = layers.Conv2D(num_filters // 2, (3, 3), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_3_3_' + index)(x3)
-    x3 = layers.Activation('relu', name=suffix + '_inception_a_activation_3_4_' + index)(x3)
-    x3 = layers.Conv2D(num_filters // 2, (3, 3), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_3_5_' + index)(x3)
-    x3 = layers.Activation('relu', name=suffix + '_inception_a_activation_3_6_' + index)(x3)
-    
-    # Branch 4
-    x4 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_a_conv2d_4_1_' + index)(x)
-    x4 = layers.Activation('relu', name=suffix + '_inception_a_activation_4_2_' + index)(x4)
-    
-    # Concatenate branches
-    x = layers.Concatenate(axis=-1, name=suffix + '_inception_a_concatenate_' + index)([x, x1, x2, x3, x4])
-    # Reduce filter size
-    x = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                      kernel_initializer=kernel_initializer,
-                      name=suffix + '_inception_a_conv2d_merge_' + index)(x)
-    
-    return x
-
-
-def _inception_block_b(x, num_filters, kernel_initializer, suffix, index):
-    # Branch 1
-    x1 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_1_1_' + index)(x)
-    x1 = layers.Activation('relu', name=suffix + '_inception_b_activation_1_2_' + index)(x1)
-    
-    # Branch 2
-    x2 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_2_1_' + index)(x)
-    x2 = layers.Activation('relu', name=suffix + '_inception_b_activation_2_2_' + index)(x2)
-    x2 = layers.SeparableConv2D(num_filters // 2, (5, 5), (1, 1), padding='same',
-                                depthwise_initializer=kernel_initializer,
-                                pointwise_initializer=kernel_initializer,
-                                name=suffix + '_inception_b_seperable_conv2d_2_3_' + index)(x2)
-    x2 = layers.Activation('relu', name=suffix + '_inception_b_activation_2_4_' + index)(x2)
-    
-    # Branch 3
-    x3 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_3_1_' + index)(x)
-    x3 = layers.Activation('relu', name=suffix + '_inception_b_activation_3_2_' + index)(x3)
-    x3 = layers.Conv2D(num_filters // 2, (3, 3), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_3_3_' + index)(x3)
-    x3 = layers.Activation('relu', name=suffix + '_inception_b_activation_3_4_' + index)(x3)
-    x3 = layers.SeparableConv2D(num_filters // 2, (7, 7), (1, 1), padding='same',
-                                depthwise_initializer=kernel_initializer,
-                                pointwise_initializer=kernel_initializer,
-                                name=suffix + '_inception_b_seperable_conv2d_3_5_' + index)(x3)
-    x3 = layers.Activation('relu', name=suffix + '_inception_b_activation_3_6_' + index)(x3)
-    
-    # Branch 3
-    x4 = layers.Conv2D(num_filters // 2, (1, 1), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_4_1_' + index)(x)
-    x4 = layers.Activation('relu', name=suffix + '_inception_b_activation_4_2_' + index)(x4)
-    x4 = layers.Conv2D(num_filters // 2, (3, 3), (1, 1), padding='same',
-                       kernel_initializer=kernel_initializer,
-                       name=suffix + '_inception_b_conv2d_4_3_' + index)(x4)
-    x4 = layers.Activation('relu', name=suffix + '_inception_b_activation_4_4_' + index)(x4)
-    x4 = layers.SeparableConv2D(num_filters // 2, (9, 9), (1, 1), padding='same',
-                                depthwise_initializer=kernel_initializer,
-                                pointwise_initializer=kernel_initializer,
-                                name=suffix + '_inception_b_seperable_conv2d_4_5_' + index)(x4)
-    x4 = layers.Activation('relu', name=suffix + '_inception_b_activation_4_6_' + index)(x4)
-    
-    # Concatenate branches
-    x = layers.Concatenate(axis=-1, name=suffix + '_inception_b_concatenate_' + index)([x, x1, x2, x3, x4])
-    # Reduce filter size
-    x = layers.Conv2D(num_filters, (1, 1), (1, 1), padding='same',
-                      kernel_initializer=kernel_initializer,
-                      name=suffix + '_inception_b_conv2d_merge_' + index)(x)
-    
-    return x
     
     
 def _shared_feature_pyramid_layers(num_pyramid_layers, input_shape, num_filters,
@@ -124,12 +17,16 @@ def _shared_feature_pyramid_layers(num_pyramid_layers, input_shape, num_filters,
                                            kernel_initializer=kernel_initializer,
                                            name=suffix + '_pyramid_down_conv2d_' + i_s + '_1_' + index))
         shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_down_activation_' + i_s + '_2_' + index))
+        shared_layers.append(layers.Conv2D(num_filters, (3, 3), (1, 1), padding='same',
+                                           kernel_initializer=kernel_initializer,
+                                           name=suffix + '_pyramid_down_conv2d_' + i_s + '_3_' + index))
+        shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_down_activation_' + i_s + '_4_' + index))
         shared_layers.append(layers.MaxPooling2D((2, 2), padding='same',
-                             name=suffix + '_pyramid_down_max_pooling_' + i_s + '_3_' + index))
+                             name=suffix + '_pyramid_down_max_pooling_' + i_s + '_5_' + index))
         x_pad_size = input_shape[0] // 4
         y_pad_size = input_shape[1] // 4
         shared_layers.append(layers.ZeroPadding2D((x_pad_size, y_pad_size),
-                             name=suffix + '_pyramid_down_padding_' + i_s + '_4_' + index))
+                             name=suffix + '_pyramid_down_padding_' + i_s + '_6_' + index))
         
         shared_down_level.append(shared_layers)
     
@@ -142,28 +39,35 @@ def _shared_feature_pyramid_layers(num_pyramid_layers, input_shape, num_filters,
                                            kernel_initializer=kernel_initializer,
                                            name=suffix + '_pyramid_up_conv2d_' + i_s + '_1_' + index))
         shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_up_activation_' + i_s + '_2_' + index))
+        shared_layers.append(layers.Conv2D(num_filters, (3, 3), (1, 1), padding='same',
+                                           kernel_initializer=kernel_initializer,
+                                           name=suffix + '_pyramid_up_conv2d_' + i_s + '_3_' + index))
+        shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_up_activation_' + i_s + '_4_' + index))
         shared_layers.append(layers.UpSampling2D((2, 2), interpolation='bilinear',
-                                                 name=suffix + '_pyramid_upsampling_' + i_s + '_3_' + index))
+                                                 name=suffix + '_pyramid_upsampling_' + i_s + '_5_' + index))
         x_crop_size = input_shape[0] // 2
         y_crop_size = input_shape[1] // 2
         shared_layers.append(layers.Cropping2D((x_crop_size, y_crop_size),
-                             name=suffix + '_pyramid_up_cropping_' + i_s + '_4_' + index))
+                             name=suffix + '_pyramid_up_cropping_' + i_s + '_6_' + index))
         
         shared_up_level.append(shared_layers)
     
     
     shared_skip = []
-    for i in range(num_pyramid_layers - 1):
+    for i in range(num_pyramid_layers):
         i_s = str(i + 1)
         shared_layers = []
         shared_layers.append(layers.Conv2D(num_filters, (1, 1), (1, 1), padding='same',
                                            kernel_initializer=kernel_initializer,
                                            name=suffix + '_pyramid_skip_conv2d_' + i_s + '_1_' + index))
         shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_skip_activation_' + i_s + '_2_' + index))
-        shared_layers.append(layers.Add(name=suffix + '_pyramid_skip_add_' + i_s + '_3_' + index))
+        shared_layers.append(layers.Conv2D(num_filters, (3, 3), (1, 1), padding='same',
+                                           kernel_initializer=kernel_initializer,
+                                           name=suffix + '_pyramid_skip_conv2d_' + i_s + '_3_' + index))
+        shared_layers.append(layers.Activation('relu', name=suffix + '_pyramid_skip_activation_' + i_s + '_4_' + index))
+        shared_layers.append(layers.Add(name=suffix + '_pyramid_skip_add_' + i_s + '_5_' + index))
         
         shared_skip.append(shared_layers)
-    
     
     return shared_down_level, shared_up_level, shared_skip
 
@@ -185,41 +89,38 @@ def feature_pyramid_layer(x, pyramid_layers, input_shape, num_filters, kernel_in
                                                                                      suffix,
                                                                                      index)
     pyramid_output = []
-    
+    pyramid_index = 0
     while True:
         x_skip = []
         x = x_input
         # Downsampling
-        for i in range(pyramid_layers):
+        for i in range(pyramid_index, pyramid_layers):
+            x_skip.append(x)
             shared_layers = shared_down_level[i]
             for j in range(len(shared_layers)):
-                x = shared_layers[j](x)
-            x_skip.append(x)        
+                x = shared_layers[j](x)        
 
-        # Remove last element, as last layer does not have a skip connection
-        del x_skip[-1]
         x_skip.reverse()
         
+
         # Upsampling
-        
-        for i in range(pyramid_layers):
-            # Pass skip data and add with main data flow
-            if i > 0:
-                shared_skip_layers = shared_skip[i - 1]
-                x_s = x_skip[i - 1]
-                for s in range(len(shared_skip_layers) - 1):
-                    x_s = shared_skip_layers[s](x_s)
-                x = shared_skip_layers[-1]([x_s, x])
-            
+        for i in range(pyramid_index, pyramid_layers):
             shared_layers = shared_up_level[i]
             for j in range(len(shared_layers)):
                 x = shared_layers[j](x)
+            
+            # Skip connection
+            shared_skip_layers = shared_skip[i]
+            x_s = x_skip[i - pyramid_index]
+            for s in range(len(shared_skip_layers) - 1):
+                x_s = shared_skip_layers[s](x_s)
+            x = shared_skip_layers[-1]([x_s, x])
         
                 
         pyramid_output.append(x)
-        
-        pyramid_layers -= 1
-        if pyramid_layers <= 0:
+
+        pyramid_index += 1
+        if pyramid_index - 1 >= pyramid_layers:
             break
     
         
@@ -239,41 +140,12 @@ def _shared_2d_branch(input_shape, kernel_initializer, downsample=False) -> kera
     shared_input = keras.layers.Input(shape=input_shape, name='input_' + suffix)
     
     x = shared_input
-    
-    target_shape = input_shape
-    if downsample:
-        # Downsample image to reduce total memory requirement
-        original_dtype = x.dtype
-        target_shape = (input_shape[0] // 2, input_shape[1] //2)
-        x = tf.image.resize(x, target_shape, method=tf.image.ResizeMethod.BILINEAR,
-                            antialias=True, name=suffix + 'image_resize_down')
-        # Cast image back to original as 'resize' returns a Tensor of float32
-        x = tf.cast(x, original_dtype, name=suffix + 'image_casting_down')
-    
-    # Pass input through inception pipeline
-    x_inc = _inception_block_a(x, num_filters=32, kernel_initializer=kernel_initializer,
-                               suffix=suffix, index='1')
-    x_inc = _inception_block_a(x_inc, num_filters=64, kernel_initializer=kernel_initializer,
-                               suffix=suffix, index='2')
-    x_inc = _inception_block_a(x_inc, num_filters=64, kernel_initializer=kernel_initializer,
-                               suffix=suffix, index='3')
-    x_inc = _inception_block_b(x_inc, num_filters=128, kernel_initializer=kernel_initializer,
-                               suffix=suffix, index='4')
-    
+
     # Pass input through multi-level feature pyramid pipeline
-    x_pyr = feature_pyramid_layer(x, pyramid_layers=3, input_shape=target_shape,
-                                  num_filters=128, kernel_initializer=kernel_initializer,
-                                  suffix=suffix, index='1')
+    x = feature_pyramid_layer(x, pyramid_layers=3, input_shape=input_shape,
+                              num_filters=128, kernel_initializer=kernel_initializer,
+                              suffix=suffix, index='1')
     
-    x = layers.Add(name=suffix + '_add_1')([x_inc, x_pyr])
-    
-    if downsample:
-        # Upsample image back to original resolution
-        target_shape = (input_shape[0], input_shape[1])
-        x = tf.image.resize(x, target_shape, method=tf.image.ResizeMethod.BILINEAR,
-                            name=suffix + 'image_resize_up')
-        x = tf.cast(x, original_dtype, name=suffix + 'image_casting_up')
-        
     shared_model = keras.models.Model(shared_input, x)
     return shared_model
 
@@ -310,11 +182,11 @@ def get_model(sa_input_shape, la_input_shape, num_classes) -> keras.Model:
                          name='sa_conv3d_1_1')(x_sa)
     x_sa = layers.Activation('relu', name='sa_activation_1_2')(x_sa)
     
-    x_sa = layers.Conv3D(32, (3, 3, 3), padding='same', kernel_initializer=kernel_initializer,
+    x_sa = layers.Conv3D(64, (3, 3, 3), padding='same', kernel_initializer=kernel_initializer,
                          name='sa_conv3d_2_1')(x_sa)
     x_sa = layers.Activation('relu', name='sa_activation_2_2')(x_sa)
     
-    x_sa = layers.Conv3D(64, (3, 3, 3), padding='same', kernel_initializer=kernel_initializer,
+    x_sa = layers.Conv3D(128, (3, 3, 3), padding='same', kernel_initializer=kernel_initializer,
                          name='sa_conv3d_3_1')(x_sa)
     x_sa = layers.Activation('relu', name='sa_activation_3_2')(x_sa)
     
@@ -333,7 +205,7 @@ def get_model(sa_input_shape, la_input_shape, num_classes) -> keras.Model:
                          name='la_conv2d_2_1')(x_la)
     x_la = layers.Activation('relu', name='la_activation_2_2')(x_la)
     
-    x_la = layers.Conv2D(64, (3, 3), padding='same', kernel_initializer=kernel_initializer,
+    x_la = layers.Conv2D(128, (3, 3), padding='same', kernel_initializer=kernel_initializer,
                          name='la_conv2d_3_1')(x_la)
     x_la = layers.Activation('relu', name='la_activation_3_2')(x_la)
       
@@ -343,11 +215,12 @@ def get_model(sa_input_shape, la_input_shape, num_classes) -> keras.Model:
     # output_sa or x_sa as input to spatial transformer
     x_la_t = spatial_target_transformer(output_sa, input_sa_affine, input_la_affine,
                                         sa_input_shape, la_input_shape)
-    x_la_t = layers.SpatialDropout2D(rate=0.5, name='la_transformation_spatial_dropout_1')(x_la_t)
     
     # Reshape from 3d to 2d (depth size is expected to be 1 after the spatial transformer)
     x_la_t = layers.Reshape((la_input_shape[0], la_input_shape[1], -1))(x_la_t)
     
+    x_la_t = layers.Dropout(rate=0.5, name='la_transformation_dropout_1')(x_la_t)
+
     x_la = layers.Concatenate(name='la_concatenate')([x_la, x_la_t])
     
     x_la = layers.Conv2D(32, (3, 3), padding='same', kernel_initializer=kernel_initializer,
@@ -361,4 +234,3 @@ def get_model(sa_input_shape, la_input_shape, num_classes) -> keras.Model:
                         [output_sa, output_la])
     
     return model
-
