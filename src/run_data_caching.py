0ca170e4565523e54cb6f9a648de787d31feb614
cgalaz01 <c.galazis20@imperial.ac.uk>
2021-07-12 15:42:04 +0100

    Updated augmentation parameters

--- src/run_data_caching.py
+++ src/run_data_caching.py
@@ -8,14 +8,18 @@ def cache_data() -> None:
     
     # Remove old cache to force recaching
     print('Removing cache...')
-    shutil.rmtree(dg.cache_directory, ignore_errors=True)
+    #shutil.rmtree(dg.cache_directory, ignore_errors=True)
     
     # Remove randomise and train/validation split so that it is easier to track
     # progress with verbose=1
     dg.train_list = dg.get_patient_list(dg.train_directory)
-    
+    import matplotlib.pyplot as plt
     print('Saving new cache...')
-    for i in dg.train_affine_generator(augment=False, verbose=1):
+    for i in dg.train_affine_generator(augment=True, verbose=1):
+        plt.imshow(i[0]['input_la'])
+        print(i[0]['input_la'].mean())
+        plt.show()
+        plt.close()
         continue
 
 if __name__ == '__main__':
