234214210f05d8d51f3d671bf57b72ec9db828ec
cgalaz01 <c.galazis20@imperial.ac.uk>
2021-09-15 11:19:21 +0100

    Clean up

--- src/run_data_anlysis.py
+++ src/run_data_anlysis.py
@@ -17,6 +17,7 @@ def get_spacing(patient_data: Dict[str, sitk.Image],
     spacing_list[0].append(sa_spacing[0])
     spacing_list[1].append(sa_spacing[2])
     spacing_list[2].append(la_spacing[0])
+    spacing_list[3].append(la_spacing[2])
     
     return spacing_list
 
@@ -31,6 +32,15 @@ def print_spacing(spacing_list: List[List[float]]) -> None:
     print('Long-Axis in-plane pixel spacing:')
     print(Counter(spacing_list[2]))
     
+    d=dict(Counter(spacing_list[2]))
+    d_view = [ (v,k) for k,v in d.items() ]
+    d_view.sort(reverse=True) # natively sort tuples by first element
+    for v,k in d_view:
+        print("%s: %d" % (k,v))
+    
+    print('Long-Axis through-plane pixel spacing:')
+    print(Counter(spacing_list[3]))
+    
     
 def get_size(patient_data: Dict[str, sitk.Image],
              size_list: List[List[float]]) -> List[List[float]]:
@@ -43,6 +53,7 @@ def get_size(patient_data: Dict[str, sitk.Image],
     size_list[2].append(sa_size[2])
     size_list[3].append(la_size[0])
     size_list[4].append(la_size[1])
+    size_list[5].append(la_size[2])
     
     return size_list
 
@@ -73,14 +84,25 @@ def get_value_range(image: sitk.Image, segmentation: sitk.Image, index) -> Tuple
     
     #np.histogram(image_n, bins=500) 
     from matplotlib import pyplot as plt 
-    plt.hist(image_n.flatten(), bins=500)
+    plt.hist(image_n.flatten(), bins='auto', range=[0, 3500])
     plt.title(str(index))
+    plt.title('Short-Axis Heart Value Distribution')
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
+    plt.ylim(0, 1000)
     plt.show()
     plt.close()
     
     min_value = np.nanmin(image_n)
     max_value = np.nanmax(image_n)
-    
+    image_n = sitk.GetArrayFromImage(image)
+    fig, ax = plt.subplots()
+    im = ax.imshow(image_n[4,:, :], cmap='hot', vmin=0, vmax=3500)
+    plt.title('Short-Axis Slice')
+    plt.axis('off')
+    fig.colorbar(im)
+    plt.show()
+    plt.close()    
     return min_value, max_value
 
 
@@ -97,8 +119,8 @@ def data_analysis() -> None:
     dg = DataGenerator()
     dg.train_list = dg.get_patient_list(dg.train_directory)
     
-    spacing_list = [[], [], []]
-    size_list = [[], [], [], [], []]
+    spacing_list = [[], [], [], []]
+    size_list = [[], [], [], [], [], []]
     
     global_min_sa = 1000
     global_max_sa = -1000
@@ -114,13 +136,14 @@ def data_analysis() -> None:
         #patient_data = dg.preprocess_patient_data(patient_data, dg.target_spacing, dg.target_size)
         size_list = get_size(patient_data, size_list)
         
+        
         min_sa, max_sa = get_value_range(patient_data['SA_ED'], patient_data['SA_ED_gt'], i)
         #print(min_sa)
         if global_min_sa > min_sa:
             global_min_sa = min_sa
         if global_max_sa < max_sa:
             global_max_sa = max_sa
-        """
+        
         min_sa, max_sa = get_value_range(patient_data['SA_ES'], patient_data['SA_ES_gt'], i)
         if global_min_sa > min_sa:
             global_min_sa = min_sa
@@ -137,7 +160,7 @@ def data_analysis() -> None:
             global_min_la = min_la
         if global_max_la < max_la:
             global_max_la = max_la
-        """
+        
         i += 1
 
     print_spacing(spacing_list)
--- src/run_data_caching.py
+++ src/run_data_caching.py
@@ -1,41 +1,28 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import shutil
 
 from data import DataGenerator
 
 
 def cache_data() -> None:
-    dg = DataGenerator(memory_cache=False)
+    dg = DataGenerator(memory_cache=False, disk_cache=False)
     
     # Remove old cache to force recaching
     print('Removing cache...')
-    #shutil.rmtree(dg.cache_directory, ignore_errors=True)
+    shutil.rmtree(dg.cache_directory, ignore_errors=True)
     
     # Remove randomise and train/validation split so that it is easier to track
     # progress with verbose=1
     dg.train_list = dg.get_patient_list(dg.train_directory)
-    import matplotlib.pyplot as plt
-    import matplotlib.patches as patches
     print('Saving new cache...')
+    
     for i in dg.train_affine_generator(augment=False, verbose=1):
-        """
-        plt.imshow(i[0]['input_sa'][..., 3])
-        width = i[0]['input_la'].shape[0] // 2 - 21 // 2
-        height = i[0]['input_la'].shape[1] // 2 - 21 // 2
-        plt.gca().add_patch(patches.Rectangle((width, height),21,21,linewidth=1,edgecolor='r',facecolor='none'))
-        plt.show()
-        plt.close()
-        plt.imshow(i[0]['input_la'][..., 0])
-        width = i[0]['input_la'].shape[0] // 2 - 21 // 2
-        height = i[0]['input_la'].shape[1] // 2 - 21 // 2
-        plt.gca().add_patch(patches.Rectangle((width, height),21,21,linewidth=1,edgecolor='r',facecolor='none'))
-        print(i[0]['input_sa'].shape)
-        plt.show()
-        plt.close()
-        """
         continue
-        
-    for i in dg.test_affine_generator_inference(verbose=1):
+    for i in dg.test_affine_generator(verbose=1):
         continue
+
 
 if __name__ == '__main__':
     cache_data()
