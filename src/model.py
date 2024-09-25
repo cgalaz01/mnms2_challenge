b59627ee30c64b3c5f7523eea4c3f47e406e9c05
Christoforos Galazis <c.galazis20@imperial.ac.uk>
2024-09-25 15:13:13 +0100

    Updated largest component selection

--- src/model.py
+++ src/model.py
@@ -1,24 +1,22 @@
 import os
 import argparse
 
-
 from typing import Union
 from pathlib import Path
 
 import numpy as np
 
-from skimage.morphology import label
 from scipy import ndimage
 
 import tensorflow as tf
 
 import SimpleITK as sitk
 
-
 from data import TensorFlowDataGenerator
 from tf.models import multi_stage_model
 from tf.losses.loss import combined_loss
 from tf.metrics.metrics import soft_dice
+
 
 class model:
     
@@ -63,27 +61,17 @@ class model:
         model.load_weights(model_path)
         
         return model
-        
-    
-    def select_largest_region(self, label_image: np.ndarray) -> np.ndarray:
-        multi_label_image, label_num = label(label_image, return_num=True,
-                                             background=0, connectivity=2)
-    
-        if label_num >= 1:
-            # Select and keep only the largest label
-            largest_label_size = 0
-            largest_index_label = 0
-            for i in range(1, label_num + 1):
-                label_size = (multi_label_image == i).sum()
-                if label_size > largest_label_size:
-                    largest_label_size = label_size
-                    largest_index_label = i
-            
-            # Remove any set labels that do not correspond to the largest segmentation
-            label_image[multi_label_image != largest_index_label] = 0
-            
-        return label_image
 
+    def select_largest_region(segmentation: sitk.Image) -> sitk.Image:
+        connected_components = sitk.ConnectedComponent(segmentation)
+        label_shape_stats = sitk.LabelShapeStatisticsImageFilter()
+        label_shape_stats.Execute(connected_components)
+        
+        if label_shape_stats.GetNumberOfLabels() > 0:
+            largest_component = max(label_shape_stats.GetLabels(), key=lambda l: label_shape_stats.GetPhysicalSize(l))
+            segmentation = sitk.Equal(connected_components, largest_component)
+                
+        return segmentation
 
     def save_predictions(self, sa_prediction: sitk.Image, la_prediction: sitk.Image,
                          patient_id: str, cardiac_phase: str,
@@ -104,18 +92,18 @@ class model:
     def test_prediction(self, input_directory: Union[str, Path],
                         output_directory: Union[str, Path]) -> None: 
         (train_gen, validation_gen,
-         test_gen, data_gen) = TensorFlowDataGenerator.get_affine_generators(batch_size=1,
-                                                                             max_buffer_size=None,
-                                                                             floating_precision='16',
-                                                                             memory_cache=False,
-                                                                             disk_cache=False,
-                                                                             test_directory=input_directory)
+         test_gen, data_gen) = TensorFlowDataGenerator.get_generators(batch_size=1,
+                                                                      max_buffer_size=None,
+                                                                      floating_precision='16',
+                                                                      memory_cache=False,
+                                                                      disk_cache=False,
+                                                                      test_directory=input_directory)
         
         model = self.load_model()
         
         threshold = -1
         
-        for data, pre_sitk, post_sitk, patient_directory, phase in data_gen.test_affine_generator_inference():
+        for data, pre_sitk, post_sitk, patient_directory, phase in data_gen.test_generator_inference():
             # Add batch dimension to each of the input values
             for key, value in data[0].items():
                 data[0][key] = np.expand_dims(value, 0)
@@ -124,10 +112,10 @@ class model:
             prediction_dict = {name: pred for name, pred in zip(model.output_names, prediction_list)}
             
             output_sa = (prediction_dict['output_sa'][0] >= threshold).astype(np.uint8)[..., 0]
-            output_sa = self.select_largest_region(output_sa)
-            output_sa *= 3  # For right ventricle label
             output_sa = np.swapaxes(output_sa, 0, -1)
             output_sa = sitk.GetImageFromArray(output_sa)
+            output_sa = self.select_largest_region(output_sa)
+            output_sa *= 3  # For right ventricle label
             
             output_sa.CopyInformation(post_sitk[0]['input_sa'])
             output_sa = sitk.Resample(output_sa, pre_sitk[0]['input_sa'],
@@ -139,11 +127,11 @@ class model:
             output_sa = output_sa_tmp
             
             output_la = (prediction_dict['output_la'][0] >= threshold).astype(np.uint8)[..., 0]
-            output_la = self.select_largest_region(output_la)
-            output_la *= 3  # For right ventricle label
             output_la = np.expand_dims(output_la, -1)   # Add 3rd dimension back
             output_la = np.swapaxes(output_la, 0, -1)
             output_la = sitk.GetImageFromArray(output_la)
+            output_la = self.select_largest_region(output_la)
+            output_la *= 3  # For right ventricle label
             
             output_la.CopyInformation(post_sitk[0]['input_la'])
             output_la = sitk.Resample(output_la, pre_sitk[0]['input_la'],
